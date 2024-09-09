e43b0b31bf62afd9d08ddbafd4ce9ab72cf0abad
Elliot Olds <5849793+elliotolds@users.noreply.github.com>
2024-09-10 01:55:17 +0200

    don't duplicate parseDollarAmount code

--- assets/js/compute_ranks.js
+++ assets/js/compute_ranks.js
@@ -1,13 +1,6 @@
-function computeRanks(data) {
-  // Function to parse dollar amounts
-  function parseDollarAmount(value) {
-    const numericValue = parseFloat(value.replace(/[$,]/g, ""));
-    if (value.includes('B')) return numericValue * 1e9;
-    if (value.includes('M')) return numericValue * 1e6;
-    if (value.includes('K')) return numericValue * 1e3;
-    return numericValue;
-  }
+import { parseDollarAmount } from './common.js';
 
+function computeRanks(data) {
   // Sort data by impact
   data.sort((a, b) => parseDollarAmount(b.impact) - parseDollarAmount(a.impact));
 
